ed45da56e951b01183a7e74bd16a0e10fad8fe49
franwanderley <franwanderley@outlook.com>
2024-11-05 16:50:13 -0300

    add react hook form

--- src/components/FormColumn.tsx
+++ src/components/FormColumn.tsx
@@ -1,17 +1,47 @@
 import { Column } from "@/interface/Column";
+import { useForm } from "react-hook-form";
+
+interface ColumnInput {
+   title: string;
+   order: number;
+   color: string;
+}
 
 export const FormColumn = ({ columns, onClose }: {columns?: Column[], onClose: () => void}) => {
+   const { register, handleSubmit } = useForm<ColumnInput>();
    const lastOrder = columns?.[columns?.length - 1]?.order || 0;
+   const onSubmit = (data: ColumnInput) => console.log(data);
+
    return (
       <div className="fixed inset-0 flex items-center justify-center z-50">
          <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
          <div className="bg-bg-secondary rounded-lg overflow-hidden shadow-lg z-10 m-4 max-h-md max-w-md p-4">
             <h1 className="text-lg mb-4 text-center">Add New Column</h1>
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
                <label htmlFor="title">Title</label>
-               <input className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" type="text" name="" id="title" placeholder="Take coffree break" />
+               <input 
+                  className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" 
+                  type="text"
+                  {...register("title", { required: true })}
+                  id="title" 
+                  placeholder="Take coffree break" 
+                  required
+               />
+               <label htmlFor="title">Color</label>
+               <input 
+                  className="bg-transparent w-full border border-border mt-2 mb-2" 
+                  type="color"
+                  {...register("color", { required: true })}
+                  id="color" 
+                  required
+               />
                <label htmlFor="status">Order</label>
-               <select className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" name="" id="status">
+               <select 
+                  className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" 
+                  id="status"
+                  {...register("order", { required: true })}
+                  required
+               >
                   {columns?.map(col => (
                      <option key={col.id} value={col.order}>{`before ${col.title}`}</option>
                   ))}
--- src/components/FormTask.tsx
+++ src/components/FormTask.tsx
@@ -1,25 +1,60 @@
 import { Column } from "@/interface/Column";
+import { SubTask } from "@/interface/SubTask";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
+interface TaskInput {
+   title: string;
+   description: string;
+   columnId: number;
+}
 
 export const FormTask = ({ columns, onClose }: {columns?: Column[], onClose: () => void}) => {
+   const { register, handleSubmit } = useForm<TaskInput>();
+   const [subtasks, setSubtasks] = useState(['']);
+   const onSubmit = (data : TaskInput) => {
+      console.log(data, subtasks);
+   };
 
    return (
       <div className="fixed inset-0 flex items-center justify-center z-50">
          <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
          <div className="bg-bg-secondary rounded-lg overflow-hidden shadow-lg z-10 m-4 max-h-md max-w-md p-4">
             <h1 className="text-lg mb-4 text-center">Add New Task</h1>
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
                <label htmlFor="title">Title</label>
-               <input className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" type="text" name="" id="title" placeholder="Take coffree break" />
+               <input 
+                  className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" 
+                  type="text" 
+                  {...register("title", {required: true})}
+                  id="title" 
+                  placeholder="Take coffree break" 
+               />
                <label htmlFor="description">Description</label>
-               <textarea className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" name="" id="description"></textarea>
+               <textarea 
+                  className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2"
+                  {...register("description", {required: true})} 
+                  id="description"
+               ></textarea>
                <label>Subtasks</label>
-               <input className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" type="text" name="" id="" placeholder="Make COfree" />
-               <button type="button" className="bg-white w-full cursor-pointer p-2 mt-3 mb-3 rounded-lg text-button">
+               {subtasks?.map((_, index) => (
+                  <input 
+                     key={index} 
+                     className="p-2 w-full bg-transparent text-gray-400 border border-border mt-2 mb-2" 
+                     type="text"
+                     onChange={({ target }) => setSubtasks(old => old?.map((value, ind) => ind === index ? target.value : value))}
+                     placeholder="Make COfree" 
+                  />
+               ))}
+               <button 
+                  type="button" 
+                  className="bg-white w-full cursor-pointer p-2 mt-3 mb-3 rounded-lg text-button" 
+                  onClick={() => setSubtasks(old => [...old, ''])}
+               >
                   + add new subtask
                </button>
                <label htmlFor="status">Status</label>
-               <select className="p-2 w-full bg-transparent text-gray-400 border border-border mb-2" name="" id="status">
+               <select {...register("columnId", {required: true})} className="p-2 w-full bg-transparent text-gray-400 border border-border mb-2" id="status">
                   {columns?.map(col => (
                      <option key={col?.id} value={col?.id}>{col?.title}</option>
                   ))}
