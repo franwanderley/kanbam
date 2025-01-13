5390935c13c0c88ce4b7c8a751269868b3560c23
franwanderley <franwanderley@outlook.com>
2025-01-13 16:20:23 -0300

    begin for drap and drop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,14 @@
 import { Task } from "@/interface/Task";
+import { useDraggable } from "@dnd-kit/core";
 
 export const Card = ({ task, onOpen }: {onOpen: () => void, task: Task | undefined}) => {
+   const {attributes, listeners, setNodeRef, transform} = useDraggable({
+      id: task?.id || 0,
+    });
    const howSubTaskIsFinish = () => task?.subtasks?.filter(sub => sub?.isFinish)?.length;
    
    return (
-      <div className="bg-bg-secondary p-2 rounded-md cursor-pointer mb-4" onClick={onOpen}>
+      <div className="bg-bg-secondary p-2 rounded-md cursor-pointer mb-4" onClick={onOpen} ref={setNodeRef} {...listeners} {...attributes}>
          <p className="text-sm mb-2">{task?.title}</p>
          <span className="text-xs text-gray-400">{`${howSubTaskIsFinish()} of ${task?.subtasks?.length} substasks`}</span>
       </div>
